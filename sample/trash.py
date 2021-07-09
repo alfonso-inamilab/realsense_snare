b191776b78fc7c274e8a438475b94e07d2655dad
java311 <ponchobalandra@gmail.com>
2021-07-09 13:04:22 +0900

    code cleaning

--- sample/trash.py
+++
@@ -1,4 +0,0 @@
-        # Remove background - Set pixels further than clipping_distance to grey
-        #grey_color = 153
-        #depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-        #bg_removed = np.where( (depth_image_3d > clipping_distance) | (depth_image_3d <= 0) , grey_color, color_image)
